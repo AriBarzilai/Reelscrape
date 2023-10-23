f5dbe99b948c89da93b5f32d522da949bfd8dd6a
Ari Barzilai <aribarzilai@gmail.com>
2023-10-23 22:14:36 +0300

    added electron-forge initial commit

--- src/main.py
+++ src/main.py
@@ -1,35 +1,43 @@
 import os
+import sys
 import cv2
 import numpy as np
 import pytesseract
 import csv
 import re
 from thefuzz import fuzz
-from thefuzz import process
 
-#sets up the directories for the data and output
+# Constants
+SIMILARITY_THRESHOLD = 70
+TIME_WINDOW = 5
+LOAD_PROGRESS_INCREMENT = 0.05
+
+def create_directory(path):
+    """Create a directory if it doesn't exist."""
+    if not os.path.exists(path):
+        os.makedirs(path)
+
 def setup_directories(base_path):
+    """Set up data and output directories."""
     data_dir = os.path.join(base_path, "data")
-    if not os.path.exists(data_dir):
-        os.makedirs(data_dir)
     videos_dir = os.path.join(data_dir, "videos")
-    if not os.path.exists(videos_dir):
-         os.makedirs(videos_dir)
-    os.chdir(videos_dir)
-    output_dir = os.path.join(base_path, "data", "output")
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
+    output_dir = os.path.join(data_dir, "output")
+
+    create_directory(data_dir)
+    create_directory(videos_dir)
+    create_directory(output_dir)
+    
     return videos_dir, output_dir
 
-# loads the video for processing
 def setup_video_capture(video_path):
+    """Initialize video capture."""
     cap = cv2.VideoCapture(video_path)
     if not cap.isOpened():
-        raise Exception("Could not open video. Please ensure there is a video at " + video_path + " and try again.")
+        raise Exception(f"Could not open video at {video_path}. Please ensure it exists and try again.")
     return cap
 
-# follows standard image processing steps to increase OCR accuracy
 def process_frame(frame):
+    """Process video frame to improve OCR accuracy."""
     # 1. Convert to Grayscale
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     # 2. Resize Image
@@ -43,8 +51,9 @@ def process_frame(frame):
     closed = cv2.morphologyEx(thresh1, cv2.MORPH_OPEN, kernel)
     return closed
 
-# a helper function to remove likely noise from extracted text
+
 def remove_noise(text):
+    """Remove noise from extracted text."""
     # The regular expression pattern below matches any character that is NOT:
     # - a letter (a-z or A-Z)
     # - a number (0-9)
@@ -60,64 +69,65 @@ def remove_noise(text):
     text = re.sub(r" +", " ", text)
     return text
 
-# extracts text from the processed frame and processes it for easier deduplication
 def extract_text(frame):
+    """Extract text from a video frame."""
     text = pytesseract.image_to_string(frame)
     text = text.replace("\n", " ")
     text = remove_noise(text)
     text = text.strip()
     return text
 
-# removes duplicate text entries (in a tuple list of (seconds, text)) in a neighborhood (of seconds) by comparing the similarity of the text
-def deduplicate_by_proximity(data, time_window=5, similarity_threshold=70):
+def deduplicate_by_proximity(data):
+    """Deduplicate text entries based on similarity and proximity."""
     to_remove = set()
     total_entries = len(data)
     for i, (timestamp, current_text) in enumerate(data):
-        #remove any empty entries (contained only noise and are now empty after removing noise in previous step)
+        # remove any empty entries (contained only noise and are now empty after removing noise in previous step)
         if not (isinstance(timestamp, str) and isinstance(current_text, str) and current_text):
             to_remove.add(i)
             continue
         # compare current text with the next few entries
-        for j in range(i+1, min(i+1+time_window, total_entries)):
+        for j in range(i+1, min(i+1+TIME_WINDOW, total_entries)):
             _, next_text = data[j]
-            if fuzz.partial_ratio(current_text, next_text) > similarity_threshold:
+            if fuzz.partial_ratio(current_text, next_text) > SIMILARITY_THRESHOLD:
                 to_remove.add(j)
 
     deduplicated = [entry for i, entry in enumerate(data) if i not in to_remove]
     return deduplicated
 
 def seconds_to_timestamp(seconds):
+    """Convert seconds to MM:SS timestamp format."""
     minutes = int(seconds / 60)
     seconds = seconds % 60
     return f"{minutes}:{seconds}"    
 
-def export_to_csv(extracted_texts, output_dir):
-    csv_path = os.path.join(output_dir, "output.csv")
+def export_to_csv(extracted_texts):
+    """Export extracted texts to CSV."""
+    print("REQUEST_OUTPUT_PATH", end='\r', flush=True)
+    csv_path = input().strip()
+    #print(csv_path)
     with open(csv_path, "w") as csv_file:
         writer = csv.writer(csv_file, lineterminator='\n')
         writer.writerow(["Time", "Text in English"])
         for seconds, text in extracted_texts:
             writer.writerow([seconds, text])
-    print("Finished exporting to csv at ", csv_path)
 
-def main():
+
+def main(video_path):
+    """Main function to extract text from video and export to CSV"""
     cwd = os.getcwd()
-    print("Extracting text from video, please be patient...")
+    #print("Extracting text from video, please be patient...", end='\r', flush=True)
     
     videos_dir, save_dir = setup_directories(cwd)
-    video_path = os.path.join(videos_dir, "1.mp4")
-    
+        
     cap = setup_video_capture(video_path)
     fps = int(cap.get(cv2.CAP_PROP_FPS))
     video_seconds = 0 # to keep track of the seconds elapsed in the video
     total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
     
-    # Used to keep track of loading progress
-    loading_milestones = {
-        int(total_frames/fps * 0.25): "25%",
-        int(total_frames/fps * 0.5): "50%",
-        int(total_frames/fps * 0.75): "75%"
-    }
+    # Used to inform the user of the progress of the program (how much of the video has been processed)
+    load_progress = 0.05
+    frame_milestone = int(total_frames/fps * load_progress)
     
     # A list to store tuples of (seconds_elapsed, extracted text)
     extracted_texts = []  
@@ -135,15 +145,21 @@ def main():
         text = extract_text(processed_frame)
         extracted_texts.append((seconds_to_timestamp(video_seconds), text))
         
-        if video_seconds in loading_milestones:
-            print(f"Processed {loading_milestones[video_seconds]}... {frame_number}/{total_frames} frames")    
+        if video_seconds == frame_milestone:
+            #print(f"Processed {int(load_progress*100)}%... {frame_number}/{total_frames} frames", end='\r', flush=True)                
+            load_progress += 0.05
+            frame_milestone = int(total_frames/fps * load_progress)
         video_seconds += 1
     cap.release()
     
     extracted_texts = deduplicate_by_proximity(extracted_texts)
-    print("Finished extracting text from video.")
-    print("Exporting to csv...")
-    export_to_csv(extracted_texts, save_dir)
+    #print("Finished extracting text from video. Exporting to csv...", flush=True)
+    export_to_csv(extracted_texts)
+    #print("Complete!\nSelect a new video to get started...", end='\r', flush=True)
 
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) < 2:
+        #print("Please provide a video path as an argument.")
+        sys.exit(1)
+    video_path = sys.argv[1]
+    main(video_path)
